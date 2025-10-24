887f8e3cf2c5f0f3c8170d198255dd0bab2b8caf
enahonguerrero <eduardonahon@icloud.com>
2025-10-24 16:28:30 +0200

    cambios

--- puntos.py
+++ puntos.py
@@ -7,5 +7,31 @@ class punto:
     def __str__(self):
         return f"{self.x}, {self.y}, este es el punto"
     
-    def vector():
-        v = A - B
+    def cuadrante(self):
+        if self.x == 0 and self.y == 0:
+            return "El punto está en el origen de coordenadas"
+        elif self.x == 0 and self.y != 0:
+            return "El punto se encuentra en el eje y"
+        elif self.x != 0 and self.y == 0:
+            return "El punto se encuentra en el eje x"
+        elif self.x > 0 and self.y > 0:
+            return "El punto se encuentra en el cuadrante : I"
+        elif self.x < 0 and self.y > 0:
+            return "El punto se encuentra en el cuadrante : II"
+        elif self.x < 0 and self.y < 0:
+            return "El punto se encuentra en el cuadrante : III"
+        elif self.x > 0 and self.y < 0:
+            return "El punto se encuentra en el cuadrante : IV"
+
+    def vector(self, otro_punto):
+        x_resultante = otro_punto.x - self.x
+        y_resultante = otro_punto.y - self.y
+        return punto(x_resultante, y_resultante)
+    
+    def distancia(self, otro_punto):
+        dx = otro_punto.x - self.x
+        dy = otro_punto.y - self.y
+        distancia = math.sqrt(dx**2 + dy**2)
+        distancia_redondeada = round(distancia, 2)
+        return distancia_redondeada
+    
