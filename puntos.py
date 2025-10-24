8b35045c3897a767a048dbdb914a6419d99821eb
enahonguerrero <eduardonahon@icloud.com>
2025-10-24 12:51:48 +0200

    Create puntos.py

---
+++ puntos.py
@@ -0,0 +1,11 @@
+import math
+class punto:
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y 
+    
+    def __str__(self):
+        return f"{self.x}, {self.y}, este es el punto"
+    
+    def vector():
+        v = A - B
